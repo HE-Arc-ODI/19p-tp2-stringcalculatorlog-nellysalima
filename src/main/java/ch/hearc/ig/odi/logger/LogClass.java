f9f584fad7de8177c975544151f3fc5816fbf3e5
nellysalima <nelly.jaquet@he-arc.ch>
2019-03-17 21:30:55 +0100

    Changing log display in console Relates #2

--- src/main/java/ch/hearc/ig/odi/logger/LogClass.java
+++ src/main/java/ch/hearc/ig/odi/logger/LogClass.java
@@ -7,15 +7,15 @@ public class LogClass {
   private static final Logger LOGGER = LogManager.getLogger(LogClass.class);
 
   public static void 	info(CharSequence message){
-   System.out.println(message);
+   System.out.println("Logger information : " + message);
   }
 
   public static void 	warn(CharSequence message){
-    System.out.println(message);
+    System.out.println("Logger warning : " + message);
   }
 
   public static void 	fatal(CharSequence message){
-    System.out.println(message);
+    System.out.println("Logger fatal : " + message);
   }
 
 }
