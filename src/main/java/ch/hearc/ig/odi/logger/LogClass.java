29c713d9e4b056921b7122c274075aed17986e7a
nellysalima <nelly.jaquet@he-arc.ch>
2019-03-17 18:31:38 +0100

    LogClass.java creation

---
+++ src/main/java/ch/hearc/ig/odi/logger/LogClass.java
@@ -0,0 +1,11 @@
+package ch.hearc.ig.odi.logger;
+
+import org.apache.logging.log4j.*;
+
+public class LogClass { 
+
+  public static Level forName(String name, int intValue) {
+    return null;
+  }
+
+}
