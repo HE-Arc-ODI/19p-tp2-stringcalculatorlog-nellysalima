532a4ad2aace2c6d016f2db8e3a006a3e90c023d
nellysalima <nelly.jaquet@he-arc.ch>
2019-03-17 19:08:42 +0100

    Declare Logger

--- src/main/java/ch/hearc/ig/odi/logger/LogClass.java
+++ src/main/java/ch/hearc/ig/odi/logger/LogClass.java
@@ -2,7 +2,9 @@ package ch.hearc.ig.odi.logger;
 
 import org.apache.logging.log4j.*;
 
-public class LogClass { 
+public class LogClass {
+
+  private static final Logger LOGGER = LogManager.getLogger();
 
   public static Level forName(String name, int intValue) {
     return null;
